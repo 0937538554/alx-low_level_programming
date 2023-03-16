d5aba0cebf252aef5908a74566ad7b903098a479
Your Name <you@example.com>
2023-03-16 04:28:51 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - with proper grammar, but the outcome is a piece of art,
+ * main - a program that print a line using the printf function,
  *
  * Return: 0 (Success)
  */
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art, \n");
+        printf("with proper grammar, but the outcome is a piece of art,\n")
         return (0);
 }
